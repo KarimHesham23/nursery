41e7cfaf170e546c3907b5931315dc2168658a1e
karimhesham <karimhesham044@gmail.com>
2025-01-25 10:04:26 +0200

    update URL

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,4 @@ const kPrimaryColor = Color(0xff001C32);
 const kSpecialColor = Color(0xff00A7F5);
 const kArvoFamily = "Arvo";
 const kSecondaryColor = Color(0xff00486A);
-const kUrl = "http://192.168.4.1/data";
+const kUrl = "http://192.168.4.1/readings";
